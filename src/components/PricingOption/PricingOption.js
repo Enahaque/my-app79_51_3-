73a1710b6a5969eb02355197a10efa13ee648a2e
Enahaque <enahaque1164@gmil.com>
2022-06-22 13:58:59 +0600

    updated

--- src/components/PricingOption/PricingOption.js
+++ src/components/PricingOption/PricingOption.js
@@ -1,3 +1,4 @@
+import { CheckCircleIcon } from '@heroicons/react/solid';
 import React from 'react';
 
 const PricingOption = (props) => {
@@ -7,6 +8,14 @@ const PricingOption = (props) => {
             <h1>{name}</h1>
             <p>
                 <span className='font-bold text-3xl bg-indigo-200 justify-center rounded-2xl'>{price}</span>
+                <span>/mo</span>
+                <div>
+                    <h3 className='text-xl text-left'>Benefits:</h3>
+                    <p  className='flex items-center text-xl'><CheckCircleIcon className='w-5 h-5 text-green-500 mr-2'></CheckCircleIcon>
+                    Awesome Features
+                    </p>
+                    
+                </div>
             </p>
             
         </div>
