58d20ff0571361cc5a827eea70fd779baf416e4f
Enahaque <enahaque1164@gmil.com>
2022-06-22 12:50:15 +0600

    updated

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -8,10 +8,10 @@ const Pricing = () => {
         {id:3,name:'Premium',price:20},
     ];
     return (
-        <div className='bg-yellow-100 p-5 mt-5'>
-            <h2 className='text-5xl text-gray-500 font-mono font-bold'>Best Pricing</h2>
+        <div className='bg-slate-400 p-5 mt-5'>
+            <h2 className='text-5xl text-white font-mono font-bold'>Best Pricing</h2>
             <p className='font-mono'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi est cum praesentium laudantium nemo sequi minima, id facere ex similique nam, repellat culpa doloremque amet.</p>
-            <div>
+            <div className='grid md:grid-cols-3 gap-5 mt-5'>
                 {
                     pricingOptions.map(pricingOption => <PricingOption
                         key={pricingOption.id}
--- src/components/PricingOption/PricingOption.js
+++ src/components/PricingOption/PricingOption.js
@@ -3,9 +3,12 @@ import React from 'react';
 const PricingOption = (props) => {
     const{name,price}=props.pricingOption;
     return (
-        <div>
+        <div className='text-3xl bg-slate-100 p-5 rounded-lg'>
             <h1>{name}</h1>
-            <h1>{price}</h1>
+            <p>
+                <span className='font-bold text-3xl bg-indigo-200 justify-center rounded-2xl'>{price}</span>
+            </p>
+            
         </div>
     );
 };
